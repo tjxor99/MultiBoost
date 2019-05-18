426fc212a3e98667100175d27e870933a458d0c3
STK <tjxor99@gmail.com>
2019-05-19 00:03:35 +0900

    Tune ABE with initialization 1

--- build/all_algs.sh
+++ build/all_algs.sh
@@ -5,7 +5,7 @@ make all
 
 dataset=$1
 # 
-train_epochs=50000
+train_epochs=10000
 if [ "$dataset" == "mnist" ]; then
 	train_epochs=30000
 fi
@@ -13,26 +13,26 @@ fi
 
 
 # TS
-bash ./ts.sh $dataset $train_epochs
+# bash ./ts.sh $dataset $train_epochs
 
 # KL-UCB, assume delta = 0.1
-bash ./klucb.sh $dataset $train_epochs
+# bash ./klucb.sh $dataset $train_epochs
 
 
 # EXP3++
 # Hyperparmaeter _c
-bash ./exp3pp.sh $dataset 0.015625 $train_epochs
+# bash ./exp3pp.sh $dataset 0.015625 $train_epochs
 # bash ./exp3pp.sh $dataset 0.03125 $train_epochs
 # bash ./exp3pp.sh $dataset 0.125 $train_epochs
 # bash ./exp3pp.sh $dataset 0.25 $train_epochs
 
 # BGE
 # bash ./bge.sh $dataset 0.25 $train_epochs
-bash ./bge.sh $dataset 0.5 $train_epochs
+# bash ./bge.sh $dataset 0.5 $train_epochs
 # bash ./bge.sh $dataset 0.75 $train_epochs
 
 # EXP3P, default params
-bash ./exp3p.sh $dataset $train_epochs
+# bash ./exp3p.sh $dataset $train_epochs
 
 # AFTL
 # Hyperparameters in order of c, alpha
@@ -40,12 +40,12 @@ bash ./exp3p.sh $dataset $train_epochs
 # bash ./aftl.sh $dataset 0.3 0.5 $train_epochs
 # bash ./aftl.sh $dataset 1 0.5 $train_epochs
 # bash ./aftl.sh $dataset 2 0.4 $train_epochs
-bash ./aftl.sh $dataset 3 0.3 $train_epochs
+# bash ./aftl.sh $dataset 3 0.3 $train_epochs
 
 # ABE
 # Hyperparameters in order of c, alpha
-# bash ./abe.sh $dataset 0.2 0.7 $train_epochs
+bash ./abe.sh $dataset 0.2 0.7 $train_epochs
 bash ./abe.sh $dataset 0.3 0.5 $train_epochs
-# bash ./abe.sh $dataset 1 0.5 $train_epochs
-# bash ./abe.sh $dataset 2 0.4 $train_epochs
-# bash ./abe.sh $dataset 3 0.3 $train_epochs
+bash ./abe.sh $dataset 1 0.5 $train_epochs
+bash ./abe.sh $dataset 2 0.4 $train_epochs
+bash ./abe.sh $dataset 3 0.3 $train_epochs
